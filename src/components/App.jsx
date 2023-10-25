5c7c53d99323ab842d6f565d05e23c3c26fe566d
Kushnierova <Katerynakushnierova@gmail.com>
2023-10-25 02:19:15 +0200

    2:19

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
+import { Profile } from './Profile/Profile';
+import user from '../json/user.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-Hi
+    <div className='container'>
+      <Profile user={user}/>
     </div>
   );
 };

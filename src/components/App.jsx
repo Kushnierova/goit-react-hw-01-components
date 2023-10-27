c9c348b57857ddc5e5de82bff61e46a9183c7367
Kushnierova <Katerynakushnierova@gmail.com>
2023-10-27 12:48:11 +0200

    12:48

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,11 @@ import user from '../json/user.json';
 export const App = () => {
   return (
     <div className='container'>
-      <Profile user={user}/>
+      <Profile   username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}/>
     </div>
   );
 };
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,7 +3,11 @@ import PropTypes from 'prop-types';
 import css from './Profile.module.css';
 
 export const Profile = ({
-  user: { username, tag, location, avatar, stats },
+  username,
+  tag,
+  location,
+  avatar,
+  stats: { followers, views, likes }
 }) => {
   return (
     <div className={css.profile}>
@@ -17,15 +21,15 @@ export const Profile = ({
       <ul className={css.stats}>
         <li className={css.item}>
           <span className={css.label}>Followers</span>
-          <span className={css.quantity}>{stats.followers}</span>
+          <span className={css.quantity}>{followers}</span>
         </li>
         <li className={css.item}>
           <span className={css.label}>Views</span>
-          <span className={css.quantity}>{stats.views}</span>
+          <span className={css.quantity}>{views}</span>
         </li>
         <li className={css.item}>
           <span className={css.label}>Likes</span>
-          <span className={css.quantity}>{stats.likes}</span>
+          <span className={css.quantity}>{likes}</span>
         </li>
       </ul>
     </div>
@@ -37,5 +41,7 @@ Profile.propTypes = {
   tag: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
   avatar: PropTypes.string.isRequired,
-  stats: PropTypes.string.isRequired,
+  followers: PropTypes.number.isRequired,
+  views: PropTypes.number.isRequired,
+  likes: PropTypes.number.isRequired,
 };

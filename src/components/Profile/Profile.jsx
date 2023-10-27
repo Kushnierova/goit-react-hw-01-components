7e25a740a3f25eee0bb0fae818a214e28d556191
Kushnierova <Katerynakushnierova@gmail.com>
2023-10-27 12:24:10 +0200

    12:24

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 import css from './Profile.module.css';
 
 export const Profile = ({
@@ -28,4 +30,12 @@ export const Profile = ({
       </ul>
     </div>
   );
+};
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.string.isRequired,
 };
